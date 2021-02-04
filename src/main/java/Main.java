9bae91fbfb1429ec6c45186a40574939096d3dd5
unknown <lawrencemboya@gmail.com>
2021-02-04 10:06:31 -0500

    SandBox and .gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap();
+        map.put(1, "Lawrence");
+        map.put(2, "John");
 
+        String firstName = map.get(1);
+        System.out.println(map.toString());
+        System.out.println(firstName);
     }
+
+
 }
---
+++ src/main/java/Sandbox.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Sandbox {
+
+    Map<Integer, String> myMap = new HashMap<>();
+
+    //Why can't I use a map in this class?
+}
